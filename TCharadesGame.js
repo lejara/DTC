dfb6aa99348f15e4b252157d6708702499a606b8
Leonel Jara <leoneljara3@gmail.com>
2020-02-14 23:06:09 -0500

    addeda ton of shit

--- TCharadesGame.js
+++ TCharadesGame.js
@@ -1,17 +1,38 @@
+//TODO: have system for word to not repeat
+//TODO: add timer
+//TODO: add a hide the word feature
+//TODO: make word bank with catagories in json format
+//TODO: fix that odd warning : Empty string passed to getElementById().
+//https://twitch-js.netlify.com/index.html
+
 const token = "gwam71i51a99gtupxqssfrzlg4o0qd";
 const username = "darkll";
 var isConnected = false;
 var connectedChannel = ""
+var chosenWord = "";
+var wordFound = false;
+
 const {
   chat,
   api
 } = new TwitchJs({
   token,
-  username
+  username,
+  log: { enabled: true },
 });
 
-function startGame() {
-  const channel = document.getElementById("Editbox1").value;
+function NextWord() {
+  PickWord();
+}
+
+function StartGame() {
+  PickWord();
+  connectTwtichChat();
+
+};
+
+function connectTwtichChat() {
+  const channel = document.getElementById("channelName").value;
 
   if (isConnected) {
     chat.reconnect().then(() => {
@@ -22,8 +43,13 @@ function startGame() {
   } else {
 
     const handleMessage = message => {
-      //console.log(message);
-      document.getElementById("output").innerHTML = message.message;
+      //console.log(message)
+      if (!wordFound && message.message != null && message.event === "PRIVMSG") {
+        document.getElementById("wb_output").innerHTML = ("<strong>" + message.username + ": </strong>" + message.message);
+        if (message.message.toLowerCase().search("\\b" + chosenWord + "\\b") != -1) {
+          WordGuessed();
+        }
+      }
     };
 
     // Listen for all events.
@@ -38,5 +64,17 @@ function startGame() {
 
   }
 
-
 };
+
+function WordGuessed() {
+  console.log("WORD GUEESED!")
+  wordFound = true;
+}
+
+function PickWord() {
+  wordFound = false;
+  chosenWord = "See";
+  document.getElementById("wb_theWord").innerHTML = chosenWord;
+  chosenWord = chosenWord.toLowerCase();
+
+}
